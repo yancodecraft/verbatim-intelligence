7eb5fa6bd4d25709fd385c4b8921c9128162ee0c
Yannick <yannick.gardiol@gmail.com>
2026-07-11 23:55:43 +0200

    refactor(frontend): replace Vue scaffold boilerplate with a product shell

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import BackendStatus from "../components/BackendStatus.vue";
 import CreateAnalysis from "../components/CreateAnalysis.vue";
-import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
-  <main>
-    <BackendStatus />
-    <CreateAnalysis />
-    <TheWelcome />
-  </main>
+	<main>
+		<BackendStatus />
+		<CreateAnalysis />
+	</main>
 </template>
